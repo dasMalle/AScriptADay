c72c3c78c78b6fbc9e6c34e000b9d7b846f07d8f
Malena Klaus <Malena Klaus>
2016-01-12 23:12:54 +0100

    renamed folders and shutdown timer script

---
+++ January/12/shutdown.py
@@ -0,0 +1,15 @@
+import datetime as dt
+import os
+
+print("hey when would you like to shutdown your computer? (hh:mm)")
+st = input()
+h, m = st.split(':')
+now = dt.datetime.now()
+later = dt.datetime(now.year, now.month, now.day, int(h), int(m))
+
+seconds = (later - now).total_seconds()
+if seconds > 0:
+    print("will be shutdown in " + str(seconds) + "seconds")
+    os.system("shutdown -s -t 1")
+else:
+    print("That's not happening today")
