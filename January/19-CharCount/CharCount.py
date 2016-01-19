bcaebc72dc2bee88764c7aebe28d40b9943594fc
dasmalle <malenaklaus@gmx.de>
2016-01-19 21:49:28 +0100

    char counter, lazy day

---
+++ January/19-CharCount/CharCount.py
@@ -0,0 +1,5 @@
+# Sorry guys but today was long and I actually needed this one, you'll get nicer scripts on the weekend
+
+print("Enter string you want to count the characters of")
+source = input()
+print("length of string is " + str(len(source)))
