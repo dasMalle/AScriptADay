c7b6440e451f790d87574b2743e7a449e3abb202
dasmalle <malenaklaus@gmx.de>
2016-02-03 22:46:23 +0100

    kind of string pcg, random hello generator

---
+++ February/February/Assets/Toolbox/HealthBar.cs
@@ -0,0 +1,41 @@
+﻿/** author: Malena Klaus
+*   date: 01.02.16 
+*   super easy health bar by using unity ui slider
+*/
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+	[Range(0, 100)] public int CurrentHealth;
+    private int initialHealth;
+    public Slider healthSlider;
+    public int damage;  //replace this by a damage script attached to the damaging gameobject
+	void Start ()
+	{
+	    initialHealth = 100;
+	    healthSlider.maxValue = initialHealth;
+	    healthSlider.minValue = 0;       
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    healthSlider.value = CurrentHealth;
+	}
+
+
+        //keep in mind that in order for this to work the damaging object needs to be checked as trigger and needs a damage script directly attached
+    void OnTriggerEnter(Collider obj)
+    {
+
+        CurrentHealth = CurrentHealth - damage;
+                    
+    }
+
+    public void Reset()
+    {
+        CurrentHealth = initialHealth;
+    }
+}
---
+++ February/February/Assets/Toolbox/PCGHelloWorld.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PCGHelloWorld : MonoBehaviour
+{
+
+    public Text str;
+    public float interval = 1;
+    public string[] hellos = new string[8] 
+    {
+      "Hello World", "Hola Mundo", "Bonjour Le Monde", "Hallo Welt", "Hej Verden", "Hej världen", "Selam Dünya", "Ciao mondo"
+    };
+
+    private float timer;
+    
+    void Start ()
+    {
+        str.text = "Hello World";
+	}
+	
+
+	void Update ()
+    {
+
+        if(timer < Time.timeSinceLevelLoad)
+        {
+            Random.seed = (int)System.DateTime.Now.Ticks;
+            int randomIndex = Random.Range(0, hellos.Length);
+            str.text = hellos[randomIndex];
+            timer = Time.timeSinceLevelLoad + interval;
+        }
+        
+    }
+}
