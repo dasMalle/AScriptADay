3e00d5d2293e7ed191bca29871744b8c56acbef4
dasmalle <malenaklaus@gmx.de>
2016-02-08 22:47:59 +0100

    rotating disco asteroid

---
+++ February/February/Assets/Toolbox/RotateAsteroid.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAsteroid : MonoBehaviour {
+
+    SpriteRenderer r;
+    public Color firstColor, second;
+    float t;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        r = GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        t = Mathf.PingPong(Time.time * speed, 1.0f);
+        transform.Rotate(new Vector3(0,0, 30) * Time.deltaTime);
+        if (r)
+        {
+            r.color = Color.Lerp(firstColor, second, t);
+        }
+    }
+}
