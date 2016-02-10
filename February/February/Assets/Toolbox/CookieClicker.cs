912d6737d86b27626dc4c9dc550ab2d266fe2263
dasmalle <malenaklaus@gmx.de>
2016-02-10 21:09:34 +0100

    cookie clicker script super light

---
+++ February/February/Assets/Toolbox/CookieClicker.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CookieClicker : MonoBehaviour {
+
+    public int clicked;
+
+    public void clickMe()
+    {
+        clicked++;
+    }
+
+
+}
