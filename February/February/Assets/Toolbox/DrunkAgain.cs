7f6dd7408076ec1b9f3dc19abf0f8203feb634c8
dasmalle <malenaklaus@gmx.de>
2016-02-12 23:42:01 +0100

    drunk scripting again, go switch

---
+++ February/February/Assets/Toolbox/DrunkAgain.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrunkAgain : MonoBehaviour {
+
+    public GameObject l;
+    public bool on = true;
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (l)
+        {
+            toggleLight(on);
+        }
+    }
+
+    public void toggleLight(bool on)
+    {
+        if(on)
+                l.SetActive(true);
+
+        else
+           l.SetActive(false);
+    }
+    
+}
