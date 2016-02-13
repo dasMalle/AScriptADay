2185a4f0d2be93db6a0883855d8c0d6cabe24e83
dasmalle <malenaklaus@gmx.de>
2016-02-13 23:50:00 +0100

    round progressbar

---
+++ February/February/Assets/Toolbox/ProgressBar.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Scrollbar))]
+public class ProgressBar : MonoBehaviour {
+
+    [SerializeField]
+    Image CircleImage;
+
+    [SerializeField]
+    [Range(0, 1)]
+    float filled = 100;
+
+    [SerializeField]
+    bool animate = true;
+    float waitTime = 30.0f;
+
+    Scrollbar scrollbar { get { return GetComponent<Scrollbar>(); } }
+
+    void Start()
+    {
+        CircleImage.type = Image.Type.Filled;
+        CircleImage.fillMethod = Image.FillMethod.Radial360;
+        CircleImage.fillOrigin = 0;
+    }
+
+    void Update()
+    {        
+        CircleImage.fillAmount = filled;
+        if(animate)
+        {
+            CircleImage.fillAmount -= 1.0f / waitTime * Time.deltaTime;
+        }
+        filled = CircleImage.fillAmount;
+    }
+
+}
