78f628cf8f43a9386f4901932d74afed8e5452d6
aaronvark <aoostdijk@gmail.com>
2016-02-15 23:25:16 +0100

    what will be your next beer casual connect 2016

---
+++ February/February/Assets/Toolbox/CC2016.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class CC2016 : MonoBehaviour {
+
+
+    public Text debugLine;
+    private List<string> beers = new List<string>();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+	// Use this for initialization
+	void Start () {
+        beers.Add("Jever");
+        beers.Add("Leffe");
+        beers.Add("Becks");
+        beers.Add("Walhalla");
+        beers.Add(" Warsteiner");
+        beers.Add(" Tuborg");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.B)) {
+           var nextBeer =  Random.Range(0, beers.Count);
+            debugLine.text = beers[nextBeer];
+        }
+	}
+}
