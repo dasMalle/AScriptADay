1792f157d4f445b0585ba22c204436d21246d0f1
dasmalle <malenaklaus@gmx.de>
2016-02-17 21:28:40 +0100

    playoneshot audio onbutton click, casual connect 2016

---
+++ February/February/Assets/Toolbox/ButtonSoundManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ButtonSoundManager : MonoBehaviour {
+    
+    public AudioClip a;
+    public AudioSource source;
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void playSound()
+    {
+        source.PlayOneShot(a); 
+    }
+}
