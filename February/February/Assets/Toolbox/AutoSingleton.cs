89f5c86a90f1d18e2d7ea8087e3800060855ba9c
dasmalle <malenaklaus@gmx.de>
2016-02-19 15:31:34 +0100

    fsm for splash game jam

---
+++ February/February/Assets/Toolbox/AutoSingleton.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public abstract class AutoSingleton : MonoBehaviour { }
+
+public abstract class AutoSingleton<T> : AutoSingleton where T : AutoSingleton
+{
+    private static bool Compare<T>(T x, T y) where T : class
+    {
+        return x == y;
+    }
+
+    #region Singleton
+
+    private static T _instance = default(T);
+
+    public static T Instance
+    {
+        get
+        {
+            if (!Compare<T>(default(T), _instance)) return _instance;
+
+            InitInstance(true);
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    public void Awake()
+    {
+        InitInstance(false);
+    }
+
+    public static void InitInstance(bool shouldInitManager)
+    {
+        Type thisType = typeof(T);
+
+        _instance = FindObjectOfType<T>();
+
+        if (Compare<T>(default(T), _instance))
+        {
+            _instance = new GameObject(thisType.Name).AddComponent<T>();
+        }
+
+        //Won't call InitManager from Awake
+        if (shouldInitManager)
+        {
+            (_instance as AutoSingleton<T>).InitManager();
+        }
+    }
+
+    public virtual void InitManager() { }
+}
---
+++ February/February/Assets/Toolbox/FSM.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FSM : MonoBehaviour {
+
+    private State currentState;
+
+    public void SwitchState(State newState)
+    {
+        currentState.EndState(this);
+        currentState = newState;
+        currentState.BeginState(this);
+    }
+
+    void Update()
+    {
+        if (currentState != null)
+        {
+            currentState.UpdateState(this);
+        }
+    }
+}
+
+public abstract class State
+{
+    public abstract void BeginState(FSM fsm);
+    public abstract void UpdateState(FSM fsm);
+    public abstract void EndState(FSM fsm);
+}
