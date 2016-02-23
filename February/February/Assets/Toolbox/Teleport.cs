526209d7cbf494a540d5a76981961e7584259846
dasmalle <malenaklaus@gmx.de>
2016-02-23 20:56:59 +0100

    script for 3 days, was sick and forgot to push

---
+++ February/February/Assets/Toolbox/Teleport.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : MonoBehaviour
+{
+    public float range = 5.0f;
+
+    void OnCollisionEnter(Collision col)
+    {
+
+        col.transform.position = Random.insideUnitSphere * 5;
+    }
+}
