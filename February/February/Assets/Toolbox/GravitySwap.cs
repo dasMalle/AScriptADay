7a148fae918869dca1a0b5079147a2c11d2a7673
dasmalle <malenaklaus@gmx.de>
2016-02-24 23:00:43 +0100

    gravity swap

---
+++ February/February/Assets/Toolbox/GravitySwap.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravitySwap : MonoBehaviour {
+
+    Vector3 gravity;
+    public bool swap = false;
+
+    void Start()
+    {
+        gravity = Physics.gravity;
+    }
+
+    void FixedUpdate()
+    {
+        Physics.gravity = gravity;
+
+        if (swap)
+        {
+            gravity.x = 0;
+            gravity.y = -25;
+            gravity.z = 0;
+        }
+    }
+}
