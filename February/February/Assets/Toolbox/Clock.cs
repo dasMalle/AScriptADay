cd203da25c2cf9bdb9791286c227d013a3f16f4b
dasmalle <malenaklaus@gmx.de>
2016-02-25 23:58:52 +0100

    added a clock

---
+++ February/February/Assets/Toolbox/Clock.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Clock : MonoBehaviour {
+
+    public Text t;
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var time = System.DateTime.Now;
+        t.text= string.Format(" {0}:{1}:{2}",  time.Hour, time.Minute, time.Second);
+    }
+}
