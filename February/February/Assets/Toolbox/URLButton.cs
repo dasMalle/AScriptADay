8310a594991fe57b7478364b98503c5885931e54
dasmalle <malenaklaus@gmx.de>
2016-02-26 23:06:50 +0100

    open url on button click

---
+++ February/February/Assets/Toolbox/URLButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class URLButton :MonoBehaviour {
+
+    public string URL = "http://malenaklaus.de";
+	public void OpenUrl()
+    {
+        Application.OpenURL(URL);
+    }
+}
