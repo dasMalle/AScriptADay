df7acab52f90aa8f9beb72b459757ce3e738b6cc
dasmalle <malenaklaus@gmx.de>
2016-02-27 23:45:38 +0100

    preparation for tomorrow

---
+++ February/February/Assets/Toolbox/PCGCave.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PCGCave : MonoBehaviour {
+
+//Random tiles for today 
+    public int[,] level;
+    public int width, height = 30;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Generate()
+    {
+        level = new int [width, height];
+
+        for(int i = 0; i< width; i++)
+        {
+            for(int j = 0; j< height; j++)
+            {
+                level[i, j] = Random.Range(0, 1);
+            }
+        }
+    }
+}
