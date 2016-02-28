9b73a121a8615bb82fd46210b15e383529be5932
dasmalle <malenaklaus@gmx.de>
2016-02-28 23:33:48 +0100

    fiddling with performance today, not much to show cause secrecy and stuff

---
+++ February/February/Assets/Toolbox/Entity.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Entity : ScriptableObject {
+
+    public string Name;
+    public int Level = 1;
+    public int Health = 2;
+    public int Strength = 1;
+    public int Magic = 0;
+    public int Defense = 0;
+    public int Speed = 1;
+    public int Damage = 1;
+    public int Armor = 0;
+    public string Weapon;
+    public Vector2 Position;
+}
---
+++ February/February/Assets/Toolbox/Performance.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Performance : MonoBehaviour {
+
+    // reducing getComponent calls
+
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = (Rigidbody2D)GetComponent(typeof(Rigidbody2D)); //this executes faster than
+        //rb = GetComponent<Renderer>().GetComponent<Collider>().GetComponent<Rigidbody>();
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
